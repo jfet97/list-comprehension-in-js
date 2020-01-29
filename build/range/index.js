5d56812c21bb90fcafc07426610b63b40d165ba9
Andrea Simone Costa <andrysimo1997@gmail.com>
2020-01-29 15:25:42 +0100

    add a test

--- build/range/index.js
+++ build/range/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   list-comprehension-in-js v0.0.14
+ *   list-comprehension-in-js v0.0.16
  *   https://github.com/jfet97/list-comprehension-in-js
  * 
  *   Copyright (c) Andrea Simone Costa (https://github.com/jfet97)
