206f56bb2a5a12a4b496966987f07a341aed8b8b
Andrea Simone Costa <andrysimo1997@gmail.com>
2020-01-29 01:15:37 +0100

    first release

---
+++ README.md
@@ -0,0 +1,2 @@
+List Comprehension in JS
+WIP
---
+++ tests/test.js
@@ -0,0 +1,53 @@
+import { Comprehension } from '../src/index';
+import { range, rangeReversed } from '../src/range';
+
+test('Runs without crashing', () => {
+  new Comprehension();
+});
+
+
+
+
+/*
+let triangles = new Comprehension(
+  (i, c1, c2) => ({ i, c1, c2 }),
+  [
+    () => range(1, 10),
+    (i) => range(1, i - 1),
+    (_, c1) => range(1, c1)
+  ],
+  [
+    (i, c1, c2) => (i ** 2 == c1 ** 2 + c2 ** 2),
+    (i, c1, c2) => (i + c1 + c2 == 24),
+  ]
+);
+
+console.log(triangles.eval());
+
+let multiplesOf17And13 = new Comprehension(
+  x => x,
+  [
+    () => range(1, Infinity),
+  ],
+  [
+    (x) => (x % 17 == 0),
+    (x) => (x % 13 == 0),
+  ]
+)
+
+let allRightTriangles = new Comprehension(
+  (i, c1, c2) => ({ i, c1, c2 }),
+  [
+    () => range(1, Infinity),
+    (i) => range(1, i - 1),
+    (_, c1) => range(1, c1)
+  ],
+  [
+    (i, c1, c2) => (i ** 2 == c1 ** 2 + c2 ** 2),
+    (i, c1, c2) => ((i + c1 + c2) % 2 == 0),
+  ]
+);
+
+// credo funzioni con qualsiasi iterabile, non solo range
+
+*/
